0755f35d6c21d4d04827d1fd623dd860b4c48cc9
Ishara-Peiris <peirisbhid.21@uom.lk>
2025-04-05 13:19:30 +0530

    connect with mongo

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ const app = express();
 //create middleware to handle json via bodyparser
 //“Hey, parse that JSON and make it available in req.body.”
 app.use(bodyParser.json());
+
+//Loads environment variables from a .env file into process.env.
+dotenv.config();
+const PORT = process.env.PORT || 5000;//IF PORT not default is 5000
+const MONGOURL = process.env.MONGO_URL;
+
+//CONNECT WITH MONGODB
+Mongoose.connect(MONGOURL).then( ()=>{
+    console.log("databse connected successful!")
+    app.listen(PORT, ()=>{
+        console.log('server is running on port ${PORT}')
+    })
+})
